7c2d62286621872c32df2a2f355ca955bf013c22
Aviscad <anthonyviscad@gmail.com>
2022-09-25 12:25:43 -0600

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/js/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name][contenthash].js",
+    clean: true,
+    assetModuleFilename: "asset/[name][ext]",
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Weather App",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+};
