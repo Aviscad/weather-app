3ffc709d6cc2f26dfc78a490998c9f98167f90dc
Aviscad <anthonyviscad@gmail.com>
2022-09-26 16:59:27 -0600

    Getting current location weather and default response

---
+++ src/js/fetchData.js
@@ -0,0 +1,26 @@
+import key from "./key.js";
+
+async function getData(
+  latitude = 13.695804686295393,
+  longitude = -89.21216453819646
+) {
+  let info;
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`
+    );
+
+    if (response.status == 200) {
+      const data = await response.json();
+      info = data;
+      return info;
+    } else {
+      console.log(response.status);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  return info;
+}
+
+export { getData };
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,34 @@
+// import "@fortawesome/fontawesome-free/js/fontawesome";
+// import "@fortawesome/fontawesome-free/js/solid";
+// import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
 import "../css/style.css";
-console.log("Hi! :)");
+import { getData } from "./fetchData";
+
+const currentLocation = navigator.geolocation.getCurrentPosition(
+  (response) => {
+    // console.log(response);
+    (async () => {
+      const locationInfo = await getData(
+        response.coords.latitude,
+        response.coords.longitude
+      );
+      showWeatherInfo(locationInfo);
+      console.log(locationInfo);
+    })();
+  },
+  (error) => {
+    (async () => {
+      const locationInfo = await getData();
+      console.log(locationInfo);
+      showWeatherInfo(locationInfo);
+    })();
+
+    console.log(error.message);
+  }
+);
+
+function showWeatherInfo(info) {
+  const weatherCard = document.getElementById("weather-card");
+  weatherCard.textContent = info.name + ", " + info.sys.country;
+}
