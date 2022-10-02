bf9b6cf9950754ebd5cb4b5fc08e822c8fb56cfd
Aviscad <anthonyviscad@gmail.com>
2022-10-02 13:33:00 -0600

    Fixing bug on snackbar

--- src/js/snackbar.js
+++ src/js/snackbar.js
@@ -1,8 +1,17 @@
 function showSnackbar(element, message, color) {
   const snack = document.querySelector(element);
+
+  //Reset classes
+  snack.classList.remove("info");
+  snack.classList.remove("warning");
+  snack.classList.remove("success");
+
+  //Set new snackbar
   snack.textContent = message;
   snack.classList.add("show");
   snack.classList.add(color);
+
+  //animation and style
   setTimeout(() => {
     snack.classList.remove("show");
     snack.classList.remove(color);
